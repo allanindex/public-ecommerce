d01e58e04826c63c2f7366726e1099e93023cad6
Allan Bispo <86433357+allanindex@users.noreply.github.com>
2024-01-02 21:38:45 -0300

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 mongoose.Promise = global.Promise
 const dotenv = require("dotenv").config()
-mongoose.connect(`mongodb+srv://bispo:${process.env.DB_PASSWORD}@ecommerce.ahxac6f.mongodb.net/?retryWrites=true&w=majority`).then(()=>{
+mongoose.connect(`your link + password`).then(()=>{
     console.log("conectado")
 }).catch((err)=>{
      console.log("erro: " + err)
